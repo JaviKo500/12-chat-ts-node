637bb1ab3e7e17d3ca6a55c55b28f3eebac8e386
JaViKo <javikogutierrez64@gmail.com>
2023-03-31 23:46:48 -0500

    connect user and disconnect

--- controllers/socket.ts
+++ controllers/socket.ts
@@ -1,11 +1,27 @@
 import { Socket } from 'socket.io';
+import { Users } from '../models/users';
+
+const users = new Users();
+
 export const socketController = ( socket: Socket ) => {
     console.log( 'connect', socket.id);
-    socket.on('disconnect', () => {
+    socket.on( 'enter-chat', ( user, callback ) => {
+        console.log(user);
+        if ( !user.name ) {
+            return callback({
+                error: true,
+                message: 'Name is required'
+            });
+        }
+        user.id = socket.id;
+        const usersList = users.addUser( user );
+        socket.broadcast.emit( 'user-list',  users.getUsers()  );
+        callback(usersList); 
     });
-    socket.on( 'send-message', ( payload, callback ) => {
-        const id = 123456;
-        callback( {id, date: new Date().getTime()} );
-        socket.broadcast.emit('send-message-client', payload);
-    }); 
+
+    socket.on('disconnect', () => {
+        const { name } = users.removeUser( socket.id ) ?? {};
+        socket.broadcast.emit('create-message', { user: 'Admin', message: `${name} out chat` });
+        socket.broadcast.emit( 'user-list',  users.getUsers()  );
+    });
 }
---
+++ public/js/socket-chat.js
@@ -0,0 +1,28 @@
+
+const params = new URLSearchParams( window.location.search );
+if ( !params.has('name') ) {
+    window.location = 'index.html';
+    throw new Error('Name is required');
+}
+
+const user = {
+    name : params.get('name')
+}
+
+const socket = io();
+socket.on( 'connect', () => {
+    socket.emit('enter-chat', user, ( resp ) => {
+        console.log(resp);
+    });
+});
+socket.on( 'disconnect', () => {
+    console.log('disconnect');
+});
+
+socket.on( 'create-message', (message) => {
+    console.log(message);
+});
+
+socket.on( 'user-list', (users) => {
+    console.log(users);
+});
